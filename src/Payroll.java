fb56dd0bb7a441e95729db5b64120f87110e7ead
Javauser <javauser@hotmail.com>
2018-11-26 07:58:34 +0800

    2018年11月25日20点44分

--- src/Payroll.java
+++ src/Payroll.java
@@ -1,8 +1,8 @@
+import excel.Excel;
 import tools.Tools;
 import window.Select;
 
 import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,7 +15,7 @@ public class Payroll {
         Select selct = new Select();
         new Tools(selct);
         registerListener(selct);
-//        new Excel();
+
     }
     //select窗口中的按钮事件
     public static void registerListener(Select s){
@@ -24,7 +24,7 @@ public class Payroll {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFileChooser chooser = new JFileChooser(".");             //设置选择器
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("excel","xls","xlsx");
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("excel文件","xls");
                 chooser.setFileFilter(filter);
                 chooser.setMultiSelectionEnabled(false);             //设为多选
                 int returnVal = chooser.showOpenDialog(s.jb_selectFile);        //是否打开文件选择框
@@ -35,9 +35,7 @@ public class Payroll {
                     String filepath = chooser.getSelectedFile().getAbsolutePath();      //获取绝对路径
 //                    System.out.println(filepath);
                     s.jt_selectFile.setText(filepath);
-
 //                    System.out.println("You chose to open this file: "+ chooser.getSelectedFile().getName());  //输出相对路径
-
                 }
             }
         });
@@ -45,6 +43,8 @@ public class Payroll {
         s.jb_start.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                File xlsFile = new File(s.jt_selectFile.getText());
+                new Excel(xlsFile);
                 String description = new String("<html><body>" +
                         "" +
                         "" +
@@ -53,35 +53,5 @@ public class Payroll {
                 s.jl_description.setText(description);
             }
         });
-    }
-}
-class MyFileFilter extends FileFilter {
-
-    String ends; // 文件后缀
-    String description; // 文件描述文字
-
-    public MyFileFilter(String ends, String description) { // 构造函数
-        this.ends = ends; // 设置文件后缀
-        this.description = description; // 设置文件描述文字
-    }
-
-    @Override
-    // 只显示符合扩展名的文件，目录全部显示
-    public boolean accept(File file) {
-        if (file.isDirectory()) return true;
-        String fileName = file.getName();
-        if (fileName.toUpperCase().endsWith(this.ends.toUpperCase())) return true;
-        return false;
-    }
-
-    @Override
-    // 返回这个扩展名过滤器的描述
-    public String getDescription() {
-        return this.description;
-    }
-
-    // 返回这个扩展名过滤器的扩展名
-    public String getEnds() {
-        return this.ends;
     }
 }
--- src/excel/Excel.java
+++ src/excel/Excel.java
@@ -3,48 +3,106 @@ package excel;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Excel {
-    public Excel(){
+    private Workbook wb = null;
+    public Excel(File xlsFile) {
         try {
-            openExcel();
-        } catch (BiffException e) {
-            e.printStackTrace();
+            readExcel(xlsFile);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
         }
+        Traversing(this.wb);
     }
-    //打开excel文件
-    public void openExcel()throws BiffException, IOException {
-        File xlsFile = new File("C:\\Users\\Javauser\\Desktop\\工资条自动生成\\9月份（个人）.xls");
-        // 获得工作簿对象
-        Workbook workbook = Workbook.getWorkbook(xlsFile);
+
+    public void readExcel(File xlsFile) throws BiffException, IOException {
+        //获取Excel对象
+        this.wb = Workbook.getWorkbook(xlsFile);
+
         // 获得所有工作表
-        Sheet[] sheets = workbook.getSheets();
-        // 遍历工作表
-        if (sheets != null)
-        {
-            for (Sheet sheet : sheets)
-            {
+//        Sheet[] sheets = wb.getSheets();
+//        //遍历工作表
+//        if (checkForm(wb)) {
+//            for (Sheet sheet : sheets) {
+//                // 获得行数
+//                int rows = sheet.getRows();
+//                // 获得列数
+//                int cols = sheet.getColumns();
+//                // 读取数据
+//                for (int row = 0; row < rows; row++) {
+//                    for (int col = 0; col < cols; col++) {
+//                        System.out.printf("%10s",sheet.getCell(col, row).getContents());
+//                    }
+//                    System.out.println();
+//                }
+//            }
+//        }
+//        wb.close();
+    }
+
+    public Boolean checkForm(Workbook wb){
+        // 获得所有工作表
+        Sheet[] sheets = wb.getSheets();
+        //遍历工作表
+        if (sheets != null) {
+            for (Sheet sheet : sheets) {
+                //查看表格是否符合规范
+                if (sheet.getCell(0, 0).getContents().equals("序号") &&
+                        sheet.getCell(1, 0).getContents().equals("账号") &&
+                        sheet.getCell(2, 0).getContents().equals("账户名称") &&
+                        sheet.getCell(3, 0).getContents().equals("工资")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+    public void Traversing(Workbook wb){
+        Sheet[] sheets = wb.getSheets();
+        //遍历工作表
+        if (checkForm(wb)) {
+            for (Sheet sheet : sheets) {
                 // 获得行数
                 int rows = sheet.getRows();
                 // 获得列数
                 int cols = sheet.getColumns();
                 // 读取数据
-                for (int row = 0; row < rows; row++)
-                {
-                    for (int col = 0; col < cols; col++)
-                    {
-                        System.out.printf("%10s", sheet.getCell(col, row)
-                                .getContents());
+                for (int row = 0; row < rows; row++) {
+                    for (int col = 0; col < cols; col++) {
+                        System.out.printf("%10s",sheet.getCell(col, row).getContents());
                     }
                     System.out.println();
                 }
             }
         }
+        wb.close();
+    }
+    public void writeExcel() throws BiffException, IOException{
+        File xlsFile = new File("jxl.xls");
+        // 创建一个工作簿
+        WritableWorkbook workbook = Workbook.createWorkbook(xlsFile);
+        // 创建一个工作表
+        WritableSheet st = workbook.createSheet("工资条", 0);
+        for (int row = 0; row < 10; row++)
+        {
+            for (int col = 0; col < 10; col++)
+            {
+                // 向工作表中添加数据
+                st.addCell(new Label(col, row, "data" + row + col));
+            }
+        }
+        workbook.write();
         workbook.close();
     }
+
 }
